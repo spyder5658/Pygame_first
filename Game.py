3f578c2b7bf6f8d6d89183517435b4dca853317c
spyder5658 <thesecretengineers007@gmail.com>
2024-12-05 19:34:45 +0545

    window created

---
+++ Game.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption("First game")
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=False
+    pygame.display.update()
