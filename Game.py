7573092ad6614ed4956cab2465d8af695416be51
spyder5658 <thesecretengineers007@gmail.com>
2024-12-05 21:07:13 +0545

    added character movement

--- Game.py
+++ Game.py
@@ -5,9 +5,30 @@ pygame.init()
 win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("First game")
 
+# for the character circle
+circle_x_position = 255
+circle_y_position = 255
+circle_radius = 15
+vel = 5
+
 run = True
 while run:
+    win.fill((0,0,0))
+
+    pygame.draw.circle(win,(255,255,255),(int(circle_x_position),int(circle_y_position)),circle_radius)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
+
+    userInput = pygame.key.get_pressed()
+    if userInput[pygame.K_LEFT]:
+        circle_x_position -= vel
+    if userInput[pygame.K_RIGHT]:
+        circle_x_position +=vel
+    if userInput[pygame.K_UP]:
+        circle_y_position -= vel
+    if userInput[pygame.K_DOWN]:
+        circle_y_position += vel
+    pygame.time.delay(15)
+
     pygame.display.update()
