69190bdf390786845a82a3b02c397cc1a4db2393
spyder5658 <thesecretengineers007@gmail.com>
2024-12-05 21:53:27 +0545

    added background

--- Game.py
+++ Game.py
@@ -2,43 +2,24 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((1000,500))
 pygame.display.set_caption("First game")
+bg_image = pygame.image.load("Background.png")
+bg = pygame.transform.scale(bg_image,(1000,500))
 
-# for the character circle
-circle_x_position = 255
-circle_y_position = 255
-circle_radius = 15
-vel_x = 10
-vel_y = 10
-
-jump =False
-
+width = 1000
+i=0
 run = True
 while run:
     win.fill((0,0,0))
-
-    pygame.draw.circle(win,(255,255,255),(int(circle_x_position),int(circle_y_position)),circle_radius)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run=False
-
-    userInput = pygame.key.get_pressed()
-    if userInput[pygame.K_LEFT] and circle_x_position >0:
-        circle_x_position -= vel_x
-    if userInput[pygame.K_RIGHT] and circle_x_position <500:
-        circle_x_position +=vel_x
-
-    if jump is False and  userInput[pygame.K_SPACE]:
-        jump = True
-    
-    if jump is True:
-        circle_y_position-=vel_y*3
-        vel_y -=1
-        if vel_y <-10:
-            jump = False
-            vel_y = 10
-    
-    pygame.time.delay(50)
+            run = False
+    win.blit(bg,(i,0))
+    win.blit(bg,(i+width,0))
+    if  i == -width:
+        win.blit(bg,(width+i,0))
+        i=0
+    i-=1
 
     pygame.display.update()
